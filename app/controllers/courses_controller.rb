9ead98e198ad7b60a0ae0f619cb43c67256d94cc
george <gkosmo1@hotmail.com>
2017-05-17 20:39:43 +0200

    playing with layout and having the new course created

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,23 @@
 class CoursesController < ApplicationController
   before_action :getCourse, only: [:show]
   def create
+    if params[:course]
+      @course = Course.new(course_params)
+
+      @course.user = current_user
+    end
+  if @course.save
+    respond_to do |format|
+      format.html { redirect_to course_path(@course) }
+      format.js  # <-- will render `app/views/reviews/create.js.erb`
+    end
+  else
+    redirect_to root_path(@course)
+  end
   end
 
   def new
+
   end
 
   def index
@@ -16,5 +30,8 @@ class CoursesController < ApplicationController
   private
   def getCourse
     @course = Course.find(params[:id])
+  end
+  def course_params
+    params.require(:course).permit(:title, :content)
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @new_course = Course.new()
     @courses = Course.all.last(5)
   end
 end
