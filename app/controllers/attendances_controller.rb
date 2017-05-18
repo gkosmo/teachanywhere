30223338c91f6ecad0da662371e5235042bd8656
george <gkosmo1@hotmail.com>
2017-05-18 16:49:41 +0200

    can say they attend

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,16 @@
 class AttendancesController < ApplicationController
 
   def create
-    
+    @event = Event.find(params[:event_id])
+    @attendance = Attendance.new({ user: current_user, event: @event})
+    @attendance.status = "Attending"
+    if @attendance.save!
+      respond_to do |format|
+        format.html { redirect_to course_path(@attendance.event.course) }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+      end
+    else
+      redirect_to root_path(@event)
+    end
   end
 end
