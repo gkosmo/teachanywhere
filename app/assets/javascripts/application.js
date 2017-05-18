ffbca303a92c6cba1fe8b0a9dd51f14aab278efb
george <gkosmo1@hotmail.com>
2017-05-18 14:56:21 +0200

    adding events to courses

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require underscore
+//= require gmaps/google
+//= require maps.js
 //= require_tree .
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,11 +3,39 @@ class EventsController < ApplicationController
   end
 
   def new
-  end
 
+  end
+  def create
+    if params[:event]
+      getCourse
+
+      @event = Event.new(event_params)
+      @event.course=@course
+      @event.user = current_user
+      STDERR.puts @event
+    end
+  if @event.save!
+    respond_to do |format|
+      format.html { redirect_to course_path(@course) }
+      format.js  # <-- will render `app/views/reviews/create.js.erb`
+    end
+  else
+    redirect_to root_path(@event)
+  end
+  end
   def index
   end
 
   def show
+  end
+  private
+  def getCourse
+    @course = Course.find(params[:course_id])
+  end
+  def getEvent
+    @event = Event.find(params[:id])
+  end
+  def event_params
+    params.require(:event).permit(:title, :content, :location, :min_attendance, :max_attendance, :date, :course_id)
   end
 end
